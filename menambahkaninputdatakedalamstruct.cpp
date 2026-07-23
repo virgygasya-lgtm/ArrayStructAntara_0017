f6c34e63bc8a8447642b2bd9ae2af0102adf147b
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-07-23 14:24:40 +0700

    menambahkaninputdatakedalamstruct.cpp

---
+++ menambahkaninputdatakedalamstruct.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+struct Orang
+{
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main()
+{
+    // Deklarasi variabel struct
+    Orang mhs;
+
+    // Mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat;
+
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur;
+
+    cout << endl;
