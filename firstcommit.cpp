05a961807f7f3a57490cf430b0070e20aec44903
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-07-23 14:03:23 +0700

    firstcommit.cpp

---
+++ firstcommit.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Deklarasi array disertai inisialisasi
+    int dataku[5] = {10, 20, 30, 40, 50};
+
+    // Deklarasi tanpa inisialisasi
+    int datamu[5];
