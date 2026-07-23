23c51848108b26275407c14b2a935ab58ecdc928
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-07-23 14:11:32 +0700

    menampilkanisiarraysecaramanual.cpp

--- menampilkanisiarraysecaramanual.cpp
+++ menampilkanisiarraysecaramanual.cpp
@@ -34,3 +34,11 @@ int main()
     cin >> datamu[4];
 
     cout << endl; 
+
+    // Menampilkan data dengan perintah satu persatu
+cout << "data pertama = " << datamu[0] << endl;
+cout << "data kedua = " << datamu[1] << endl;
+cout << "data ketiga = " << datamu[2] << endl;
+cout << "data keempat = " << datamu[3] << endl;
+cout << "data kelima = " << datamu[4] << endl;
+cout << endl;
